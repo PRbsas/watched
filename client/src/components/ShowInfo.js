7b872f17bf72d8e89ea911d5bf928364413128de
PRbsas <paularamirez0316@gmail.com>
2018-05-08 10:29:32 -0400

    Render show info in ShowInfo component

--- client/src/components/ShowInfo.js
+++ client/src/components/ShowInfo.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import { Box, Text, ButtonCircle } from 'rebass'
+import { Box, Text, ButtonCircle, Divider, Heading } from 'rebass'
 
 const ShowInfo = ({ showInfo, addShow }) => {
   return (
-    <Box width={1} mt={[ 30, 0 ]}>
-      <Text>{showInfo.overview}</Text>
+    <Box width={1} mt={[ 30, 0 ]} pl={10}>
+      <Heading>{showInfo.title}</Heading>
+      <Divider />
+      <Text>Genres: <strong>{showInfo.genres.map(genre => <span>{genre} </span>)}</strong></Text>
+      <Text mt={1}>Year: <strong>{showInfo.year}</strong></Text>
+      <Text mt={1}>Status: <strong>{showInfo.status}</strong></Text>
+      <Text mt={1}>Episodes: <strong>{showInfo.aired_episodes}</strong></Text>
+      <Text mt={3}>{showInfo.overview}</Text>
       <ButtonCircle type='submit' bg='black' mt={15} onClick={addShow}>Add</ButtonCircle>
     </Box>
   )
