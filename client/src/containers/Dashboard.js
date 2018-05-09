2fb91f1896deb11afa4dd33f5b23890efc19d273
PRbsas <paularamirez0316@gmail.com>
2018-05-08 20:21:44 -0400

    Sort shows according to number of likes

--- client/src/containers/Dashboard.js
+++ client/src/containers/Dashboard.js
@@ -10,7 +10,14 @@ class Dashboard extends Component {
       <Container mt={5}>
         <Stats count={this.props.collection.length} />
         <Flex flexWrap='wrap' width={1}>
-          {this.props.collection.map(show => <CollectionInfo key={show.id} info={show} />)}
+
+          {/* {this.props.collection.map(show => <CollectionInfo key={show.id} info={show} />)} */}
+
+          {
+            this.props.collection.sort((a, b) => {
+              return b.likes - a.likes
+            }).map(show => <CollectionInfo key={show.id} info={show} />)
+          }
         </Flex>
       </Container>
     )
